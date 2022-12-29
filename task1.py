87eede0f347b225b7d1d40e723e708d443e15f67
Natasha Eater <natalya.iter@yandex.ru>
2022-12-29 18:08:52 +0300

    1

--- task1.py
+++ task1.py
@@ -1,3 +1,23 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+from decimal import Decimal
+
+print('Задайте точность рандомного вычисления формате 0.0001:')
+num3 = float(input())
+
+print('Для примера будем использовать умножение двух чисел.')
+
+print('Введите первое число')
+num1 = float(input())
+print('Введите второе число')
+num2 = float(input())
+
+res =num1*num2
+
+def num_d(num, d):
+    number = Decimal(num)
+    return number.quantize(Decimal(f"{d}"))
+
+print(num_d(res,num3))
