095e1aca538a6ade3e374b72bbdc545fa75bce2a
Natasha Eater <natalya.iter@yandex.ru>
2022-12-29 19:38:58 +0300

    5

--- task4.py
+++ task4.py
@@ -2,3 +2,40 @@
 # записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+
+from random import randint
+
+print('Введите степень k')
+num = int(input())
+print()
+
+i = num
+res = ''
+while i > -1:
+    if (i == num):
+        a = randint(1, 100)
+        if a == 1:
+           res = 'x^' + str(i) 
+        res = str(a) + '*x^' + str(i)
+    elif i > 1:
+        a = randint(0, 100)
+        if a == 1:
+            res = res + ' + x^' + str(i)
+        elif a > 1:
+            res = res + ' + ' + str(a) + '*x^' + str(i)
+    elif i == 1:
+        a = randint(0, 100)
+        if a > 1:
+            res = res + ' + ' + str(a) + '*x'
+        elif a == 1:
+            res = res + ' + x'
+    else:
+        a = randint(0, 100)
+        if a > 1:
+            res = res + ' + ' + str(a) + ' = 0'
+        else: res = res + ' = 0'
+    i = i - 1
+data = open('text.txt', 'a')
+data.write(res)
+data.close()
--- task5.py
+++ task5.py
@@ -1,2 +1,14 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов
+
+
+with open("text.txt","r") as file:
+    data1 = file.read()
+with open("text2.txt","r") as file:
+    data2 = file.read()
+
+st = data1.split(" = ")
+result = st[0] + " + " + data2
+
+with open("task5res.txt", "w") as file2:
+    file2.write(result)
